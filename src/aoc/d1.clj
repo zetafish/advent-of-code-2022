26485db1a1c63431506092571d637af8e801f396
Endymion Kasanardjo <zetafish@gmail.com>
2022-12-01 20:17:09 +0100

    day 1

---
+++ src/aoc/d1.clj
@@ -0,0 +1,46 @@
+(ns aoc.d1
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def example
+  ["1000"
+   "2000"
+   "3000"
+   ""
+   "4000"
+   ""
+   "5000"
+   "6000"
+   ""
+   "7000"
+   "8000"
+   "9000"
+   ""
+   "10000"])
+
+(def input (str/split-lines (slurp (io/resource "input1.txt"))))
+
+(defn sum-elves
+  [col]
+  (->>
+   col
+   (partition-by #{""})
+   (remove #{[""]})
+   (map (partial map #(Integer/parseInt %)))
+   (map (partial apply +))))
+
+(defn top
+  [n col]
+  (->>
+   col
+   sort
+   reverse
+   (take n)
+   (reduce +)))
+
+(top 1 (sum-elves example))
+(top 3 (sum-elves example))
+
+(top 1 (sum-elves input))
+(top 3 (sum-elves input))
+
